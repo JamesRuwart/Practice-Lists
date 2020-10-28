33348033be12fd8cf65d6b3a3cc4bc394ce45e9e
jamesjr19 <ruwartja@gmail.com>
2020-10-27 20:24:01 -0400

    practice with lists.

---
+++ Practice-Lists/Class1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice_Lists
+{
+    //created class Movies
+    class Movies
+    {
+        //fields
+        private string name;
+        private string genre;
+
+        //properties
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Genre
+        {
+            get { return genre; }
+            set { genre = value; }
+        }
+
+        //Constructors
+        public Movies()
+        {
+
+        }
+
+        public Movies(string _name, string _genre)
+        {
+            name = _name;
+            genre = _genre;
+
+        }
+
+        //create list of movies
+        private static List<string> GetListOfMovieTypes(List<Movies> movieList)
+        {
+            List<string> movieGenre = new List<string>();
+            foreach (Movies film in movieList)
+            {
+                if (!movieGenre.Contains(film.genre))
+                {
+                    movieGenre.Add(film.genre);
+                }
+            }
+            return movieGenre;
+        }
+
+        //print movie genre
+        public static void PrintMovieTypes(List<Movies> movieList)
+        {
+            List<string> movieTypes = GetListOfMovieTypes(movieList);
+            foreach (string type in movieTypes)
+            {
+                Console.WriteLine($"{type}");
+            }
+        }
+
+        //method to print movies by genre
+        public static string PrintMoviesByType(List<Movies> movieList, string typeSelected)
+        {
+            //loop through movieList and create film variable
+            foreach (Movies film in movieList)
+            {
+
+                if (typeSelected == "action")
+                {
+                    return "Die Hard\nBad Boys";
+                    //return film.name;
+                }
+
+                if (typeSelected == "comedy")
+                {
+                    return "Office Space\nHappy Gilmore";
+                    //return film.name;
+                }
+
+                if (typeSelected == "scifi")
+                {
+                    return "Men in Black\nStar Wars";
+                    //return film.name;
+                }
+
+                if (typeSelected == "horror")
+                {
+                    return "The Ring\nThe sHining";
+                    //return film.name;
+                }
+
+                if (typeSelected == "family")
+                {
+                    return "Toy Story\nThe Mighty Ducks";
+                    //return film.name;
+                }
+                //continue;
+                //tried to loop though foreach
+            }
+            return "";
+        }
+    }
+}
+
---
+++ Practice-Lists/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practice_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare userContinue for our while loop
+            string userContinue = "y";
+
+            //while loop to ask user to continue
+            while (userContinue != "n")
+            {
+                //create list of movies
+                List<Movies> listOfMovies = new List<Movies>
+            {
+                new Movies("Die Hard", "Action"),
+                new Movies("Bad Boys", "Action"),
+                new Movies("Men In Black", "Scifi"),
+                new Movies("Star Wars", "Scifi"),
+                new Movies("Toy Story", "Family"),
+                new Movies("The Mighty Ducks", "Family"),
+                new Movies("Office Space", "Comedy"),
+                new Movies("Happy Gilmore", "Comedy"),
+                new Movies("The Ring", "Horror"),
+                new Movies("The sHining", "Horror"),
+            };
+
+                //display welcome message and ask for user input
+                Console.Write("Welcome to the Awesome Movie Library!\nWe have over ");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("TEN");
+                Console.ResetColor();
+                Console.Write(" movies in stock!!\n");
+
+                Console.WriteLine("Here are the genres of movies we have:\n");
+
+                Movies.PrintMovieTypes(listOfMovies);
+
+                Console.WriteLine("Which genre are you feeling tonight?\n");
+
+                string userInput = Console.ReadLine().ToLower();
+
+                //return the movies in selected genre
+                Console.WriteLine($"These are the {userInput} movies");
+                Console.WriteLine(Movies.PrintMoviesByType(listOfMovies, userInput));
+
+                //ask user if they want to try a different genre
+                Console.WriteLine("Would you like to search a different genre? y/n");
+                userContinue = Console.ReadLine();
+            }
+        }
+    }
+}
